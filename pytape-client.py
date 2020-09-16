d2987eef80df1a1b363890795572d2fddcdfc672
caxapyk <saharuk.alexander@gmail.com>
2020-09-16 13:57:34 +0300

    in progress

--- pytape-client.py
+++ pytape-client.py
@@ -5,35 +5,101 @@
 
 import argparse
 import socket
+import sys
 
 
 class Client():
     def connect(self, host, port):
+        # Create an AF_INET, STREAM socket (TCP)
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect((host, port))
-            sock.sendall(b'STATUSTAPE /dev/tape0')
-
-            data = sock.recv(2048)
-            print('Received', data.decode('utf-8'))
-            sock.close()
-
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error as e:
-            sock.close()
-            print('Connection error: %s' % e)
+            print("Failed to create socket. Error: %s" % e)
+            sys.exit()
+
+        # Connect to server
+        try:
+            print("Connecting to server...")
+            self.sock.connect((host, port))
+        except socket.error as e:
+            print("Failed to connect to server %s:%s. "
+                  "Error: %s" % (host, port, e))
+            sys.exit()
+
+        print("Connection established!")
+
+    def disconnect(self):
+        self.sock.close()
+
+    def send_command(self, command):
+        try:
+            self.sock.sendall(command)
+            response = self.sock.recv(1024).decode('utf-8')
+
+            return response
+        except socket.error as e:
+            print("Failed to send command. Error: %s" % e)
+            sys.exit()
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='PyTape Client 2020 Сахарук Александр')
-    parser.add_argument('-s', '--server', action='store',
-                        default='localhost', help='Адрес сервера')
+        description='PyTape Client 2020 Sakharuk Alexander')
+    parser.add_argument('-s', '--host', action='store',
+                        default='localhost', help='Server hostname')
     parser.add_argument('-p', '--port', action='store',
-                        default=50077, help='Порт сервера')
+                        default=50077, help='Server port')
+
     args = parser.parse_args()
 
+    cmd_parser = argparse.ArgumentParser(
+        description='PyTape Client 2020 Sakharuk Alexander')
+
+    cmd_parser.add_argument('command', choices=[
+        'backup',
+        'exit',
+        'list',
+        'rewind',
+        'status'])
+
     client = Client()
-    client.connect(args.server, args.port)
+    client.connect(args.host, args.port)
+
+    # Start interactive console
+    while True:
+        cmd = input("> ")
+        try:
+            if(len(cmd) > 0):
+                iargs = cmd_parser.parse_args(cmd.split())
+            else:
+                continue
+        except SystemExit:
+            continue
+
+        if iargs.command == 'backup':
+            bdir = client.send_command(b'GETBDIR')
+            cmd = input("Backup directory on server [%s]: " % bdir)
+            if(cmd):
+                print(client.send_command(bytes('BACKUPSPECDIR %s'.encode('utf-8') % bdir)))
+            else:
+                print(client.send_command(b'BACKUP'))
+
+
+        elif iargs.command == 'list':
+            print(client.send_command(b'LIST'))
+        elif iargs.command == 'rewind':
+            print(client.send_command(b'REWIND'))
+        elif iargs.command == 'status':
+            print(client.send_command(b'STATUS'))
+
+        elif iargs.command == 'exit':
+            client.disconnect()
+            break
+        else:
+            print("Unknown command")
+            continue
+
+        continue
 
 
 if __name__ == "__main__":
--- pytape-server.py
+++ pytape-server.py
@@ -7,74 +7,97 @@ import argparse
 import os
 import socket
 import subprocess
+import sys
 
 
-class CommandHandler():
-    # protocol commands
-    C_PROTOCOL = {
-        'STATUS': 'mt status',
-        'STATUSTAPE': 'mt status {}',
-        'BACKUP': '',
-        'RESTORE': ''
-    }
+class Server():
+    def __init__(self):
+        self.backup_dir = "/root"
+        # protocol commands
+        self.base_commands = {
+            'STATUS': 'mt status',
+            'LIST': 'tar tzv && mt bsf 2 && mt fsf',
+            'BACKUP': 'mt eom && tar czv %s -C %s' % (self.get_bdir(), self.get_bdir()),
+            'BACKUPSPECDIR': 'mt eom && tar czv {} -C {}',
+            'REWIND': 'mt rewind'
+        }
+
+        self.ext_commands = {
+            'GETBDIR': self.get_bdir(),
+        }
+
+    def run(self, host, port):
+        # Create an AF_INET, STREAM socket (TCP)
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setblocking(True)
+        except socket.error as e:
+            print("Failed to create socket. Error: %s" % e)
+            sys.exit()
+        # Bind socket to local host and port
+        try:
+            sock.bind((host, port))
+        except socket.error as e:
+            print("Failed to bind socket. Error: %s" % e)
+            sys.exit()
+        # Start listening on socket
+        sock.listen(1)
+        print("Server started on %s:%s\n"
+              "Waiting for client connections..." % (host, port))
+
+        # Start main loop for waiting connections
+        while True:
+            conn, addr = sock.accept()
+            print('Client connected by', addr)
+
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+
+                args = data.decode('utf-8').split()
+                response = self._exec(args)
+
+                conn.sendall(response)
+
+        sock.close()
 
     def _exec(self, args):
         command = None
 
-        if(args[0] in self.C_PROTOCOL):
+        if(args[0] in self.base_commands):
             if(len(args) > 1):
-                command = self.C_PROTOCOL[args[0]].format(*args[1::])
+                command = self.base_commands[args[0]].format(*args[1::])
             else:
-                command = self.C_PROTOCOL[args[0]]
+                command = self.base_commands[args[0]]
+
+            try:
+                proc = subprocess.Popen(command, shell=True,
+                                        stderr=subprocess.PIPE,
+                                        stdout=subprocess.PIPE)
+                if proc:
+                    out = proc.stdout.read()
+                    stderr = proc.stderr.read()
+                    if (stderr):
+                        out += stderr
+                    if(len(out) == 0):
+                        out = b'Done'
+
+                    return out
+            except OSError as e:
+                print('Could not start subprocess on server. Error: %s' % e)
+                return b'Could not start subprocess on server.'
+
+        elif(args[0] in self.ext_commands):
+            return bytes(self.ext_commands[args[0]].encode("utf-8"))
         else:
-            return b'Unknown command'
+            return b'Server could not recognize command.'
 
-        with subprocess.Popen(command, shell=True,
-                              stderr=subprocess.PIPE,
-                              stdout=subprocess.PIPE) as proc:
-            if proc:
-                out = proc.stdout.read()
-                stderr = proc.stderr.read()
+    def set_backup_dir(self, path):
+        self.backup_dir = path
 
-                if (stderr):
-                    out += stderr
-
-            return out
-
-
-class Server():
-    def __init__(self, host, port):
-        self.SERVER_HOST = host
-        self.SERVER_PORT = port
-
-    def run(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.bind((self.SERVER_HOST, self.SERVER_PORT))
-            sock.listen(1)
-
-            handler = CommandHandler()
-
-            # main loop
-            while True:
-                try:
-                    conn, addr = sock.accept()
-                    print('Connected by', addr)
-
-                    while True:
-                        data = conn.recv(1024)
-                        if not data:
-                            break
-
-                        args = data.decode('utf-8').split()
-                        response = handler._exec(args)
-
-                        conn.sendall(bytes(response))
-
-                    conn.close()
-
-                except socket.error as e:
-                    conn.close()
-                    print('Client connection error: %s' % e)
+    def get_bdir(self):
+        return self.backup_dir
 
     def install(self):
         u = ("[Unit]\n"
@@ -97,30 +120,38 @@ class Server():
             unit.write(u)
             unit.close()
         except os.error as e:
-            print('Could not install server as service: %s' %
-                  e, '\nTry to run from superuser.')
+            print('Could not install server as service. Error: %s' %
+                  e, '\n\nTry to run from superuser.')
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='PyTape Server 2020 Sakharuk Alexander')
+    parser.add_argument('--dir', action='store',
+                        default='/root/',
+                        help='Listen on interfaces [default is /root/]')
     parser.add_argument('--host', action='store',
-                        default='', help='Run on hostname [default is any host]')
+                        default='',
+                        help='Listen on interfaces [default is any]')
     parser.add_argument('--port', action='store',
-                        default=50077, help='Run on port [default is 50077]')
-    parser.add_argument('--run', action='store_true', help='Run server')
+                        default=50077,
+                        help='Run on port [default is 50077]')
+    parser.add_argument('--tape',
+                        default='/dev/nst0',
+                        help='Tape device [default is /dev/nst0]')
     parser.add_argument('--install', action='store_true',
                         help='Install server as systemd daemon')
     args = parser.parse_args()
 
-    server = Server(args.host, args.port)
+    server = Server()
+    server.set_backup_dir(args.dir)
 
-    if(args.run):
-        server.run()
-    elif(args.install):
+    os.environ["TAPE"] = args.tape
+
+    if(args.install):
         server.install()
     else:
-        parser.print_help()
+        server.run(args.host, args.port)
 
 
 if __name__ == "__main__":
